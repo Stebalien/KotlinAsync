e9f56e94fe449fa7ff34194df9a3a43fadb9ef89
Steven Allen <steven@stebalien.com>
2014-03-06 15:04:03 -0500

    Separate asyncMain.

--- src/main.kt
+++ src/main.kt
@@ -6,42 +6,47 @@
 
 import java.util.concurrent.TimeUnit
 
-fun main(args: Array<String>): Unit {
-    async<Unit> {
-        await(TrivialPromise(1)) {
-
-        // Kotlin *claims* it can't determine Int (but it can and does in the error message...)
-        await<Unit>(atry<Unit> {
-            println("before")
-            throw IllegalStateException("Testing")
-        }.catch(javaClass<IllegalStateException>()) {
-            println("here")
-        }.finally {
-            println("at last")
-        }) {
-
-        println("first")
-        await<Unit>(sleep(1000)) {
-        println("second")
-        await<Unit>(unblock{Thread.sleep(1000)}) {
-        println("third")
-
-        await<Unit>(aforeach(1..10) {
-            if (it == 5) acontinue()
-            if (it == 8) abreak()
-            println(it)
-        }) {
-
-        val linesPromise = java.io.File("/etc/shells").readLinesAsync()
-        println("reading lines")
-        await(linesPromise) { lines ->
-
-        await<Unit>(aforeach(lines) { line ->
-            println(line)
-        }) {
-        println("done")
-    }}}}}}}}
+fun main(args: Array<String>) {
+    asyncMain(args) otherwise {
+        throw it
+    }
 }
+
+fun asyncMain(args: Array<String>) = async<Unit> {
+    await(TrivialPromise(1)) {
+
+    // Kotlin *claims* it can't determine Int (but it can and does in the error message...)
+    await<Unit>(atry<Unit> {
+        println("before")
+        throw IllegalStateException("Testing")
+    }.catch(javaClass<IllegalStateException>()) {
+        println("here")
+        //throw it
+    }.finally {
+        println("at last")
+    }) {
+
+    println("first")
+    await<Unit>(sleep(1000)) {
+    println("second")
+    await<Unit>(unblock{Thread.sleep(1000)}) {
+    println("third")
+
+    await<Unit>(aforeach(1..10) {
+        if (it == 5) acontinue()
+        if (it == 8) abreak()
+        println(it)
+    }) {
+
+    val linesPromise = java.io.File("/etc/shells").readLinesAsync()
+    println("reading lines")
+    await(linesPromise) { lines ->
+
+    await<Unit>(aforeach(lines) { line ->
+        println(line)
+    }) {
+    println("done")
+}}}}}}}}
 /*
 THIS IS A PROTOTYPE/MOCKUP!!!
 
